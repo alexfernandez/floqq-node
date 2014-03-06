7ceb78ea7d74a9f16728cc47f8806ff4c7b2e1c3
Alex Fernández <alex.fernandez@mediasmart.es>
2014-03-06 14:46:00 +0100

    Added project contents.

---
+++ server-socket.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var net = require('net');
+var port = 1702;
+var server = net.createServer(function(connection) {
+	console.log('Connection open');
+	connection.write('Hello?\r\n');
+	connection.on('data', function(data) {
+		if (String(data).trim() != 'hello') {
+			connection.write('ERROR\r\n');
+		} else {
+			connection.end('world\r\n');
+		}
+	});
+	connection.on('error', function(error) {
+		console.log('Error: %s', error);
+	});
+	connection.on('end', function() {
+		console.log('Connection closed');
+	});
+});
+server.listen(port);
